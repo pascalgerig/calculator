a1a8e3c56635cab41df63c8697dcd4fc25dcfdb4
Pascal Gerig <pgerig@fastforward.ch>
2019-04-27 17:18:32 +0200

    properly kill process if gui is closed

--- src/main/java/CalcView.java
+++ src/main/java/CalcView.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.LinkedList;
 import java.util.Observer;
 import javax.swing.*;
@@ -51,6 +52,13 @@ public class CalcView extends JFrame implements Observer, CalculatorView{
         this.setupRight();
         panel.add(leftPanel);
         panel.add(rightPanel);
+
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
     }
 
     protected void setupleft() {
